2be64d5606f7d3864c06b05f2f3852323e117603
Soesh Cooray <soeshcooray@gmail.com>
2026-01-14 09:03:11 +0530

    .

---
+++ config.js
@@ -0,0 +1,9 @@
+// Contentful Configuration
+// DO NOT commit this file to version control
+const CONFIG = {
+    CONTENTFUL: {
+        SPACE_ID: 'cr2v5nbc0rx2',
+        ACCESS_TOKEN: 'qmEA1IrWGYo8EfA_C8tK9wZqWUS8kLWt4QymemY1O4k',
+        CONTENT_TYPE: 'news'
+    }
+};
